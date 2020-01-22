5f0d17ee37ddbe86412045e97078ae2e1383d454
Suzanna <nor_suzanna_razali@yahoo.com>
2020-01-22 21:29:17 +0800

    Updates

---
+++ precache-manifest.04ec918e29e08ccb61f8799692fa09cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa7dea48629a95574483f0562180207",
+    "url": "/custom-repo/index.html"
+  },
+  {
+    "revision": "8df9e16a1625eac2dff7",
+    "url": "/custom-repo/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "2aa58ed2b849719ab79e",
+    "url": "/custom-repo/static/css/main.3a6bdba4.chunk.css"
+  },
+  {
+    "revision": "8df9e16a1625eac2dff7",
+    "url": "/custom-repo/static/js/2.27fd2583.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/custom-repo/static/js/2.27fd2583.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aa58ed2b849719ab79e",
+    "url": "/custom-repo/static/js/main.a7426415.chunk.js"
+  },
+  {
+    "revision": "34014bcafbba4b529c61",
+    "url": "/custom-repo/static/js/runtime-main.d274db67.js"
+  }
+]);
